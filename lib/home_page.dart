5648f6d40c398ede17cddf673a3239db9465d16d
Fernando Dias <fernandodx@hotmail.com>
2019-09-12 23:21:23 -0300

    Initial commit

---
+++ lib/home_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Queimadas"),
+          bottom: TabBar(tabs: [
+            Text("PAGE 1"),
+            Text("PAGE 2")
+          ]),
+        ),
+        body: TabBarView(children: [
+          Container(
+            color: Colors.green,
+          ),
+          Container(
+            color: Colors.greenAccent,
+          )
+        ]),
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                accountName: Text("Nome"),
+                accountEmail: Text("E-mail"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage("https://avatarfiles.alphacoders.com/115/115265.png"),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
